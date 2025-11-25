d2fccaadc88324f77e3027142297d2e1e684d5bf
Marcelo Nascimento <marcelo.ngomes@hotmail.com>
2025-11-24 23:52:43 -0300

    Adiciona um comentário ao código

--- src/dia01/HelloWorld.java
+++ src/dia01/HelloWorld.java
@@ -2,6 +2,10 @@ package dia01;
 
 public class HelloWorld {
 
+	/*
+	 * Esse programa gera uma mensagem ao usuário
+	 */
+
 	public static void main(String[] args) {
 
 		System.out.println("Hello World!");
