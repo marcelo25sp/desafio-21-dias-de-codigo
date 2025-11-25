f9156ac5b5cf99942d355e666b565d283110035c
Marcelo Nascimento <marcelo.ngomes@hotmail.com>
2025-11-25 00:00:01 -0300

    Cria o pacote dia02 e a classe Ex001Soma com a estrutura inicial

---
+++ src/dia02/Ex001Soma.java
@@ -0,0 +1,17 @@
+package dia02;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Ex001Soma {
+
+	public static void main(String[] args) {
+		
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		sc.close();
+
+	}
+
+}
