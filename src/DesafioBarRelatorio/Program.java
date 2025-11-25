eeeb360002a1789921aa56dd038479a25e900972
Marcelo Nascimento <marcelo.ngomes@hotmail.com>
2025-11-25 11:34:58 -0300

    Solicita dados de entrada do usuário sobre o consumo

--- src/DesafioBarRelatorio/Program.java
+++ src/DesafioBarRelatorio/Program.java
@@ -6,10 +6,19 @@ import java.util.Scanner;
 public class Program {
 
 	public static void main(String[] args) {
-		
+
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
-		
+
+		System.out.print("Genero:(M/F) ");
+		char genero = sc.next().charAt(0);
+		System.out.print("Quantidade de cervejas: ");
+		int cervejas = sc.nextInt();
+		System.out.print("Quantidade de refrigerantes: ");
+		int refrigerantes = sc.nextInt();
+		System.out.print("Quantidade de espetinhos: ");
+		int espetinhos = sc.nextInt();
+
 		sc.close();
 	}
 
