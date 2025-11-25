604a7fb7ad0f49f10dd796647d5d7dbd3357328d
Marcelo Nascimento <marcelo.ngomes@hotmail.com>
2025-11-25 11:27:12 -0300

    Cria o método valorTotal que retorna o valor final a ser pago com ingresso e couvert incluso ao consumo

--- src/DesafioBarRelatorio/Bar.java
+++ src/DesafioBarRelatorio/Bar.java
@@ -15,4 +15,16 @@ public class Bar {
 		return cerveja + refrigerante + espetinho;
 	}
 
+	public double ingresso() {
+		if (genero == 'M') {
+			return 10.0;
+		} else {
+			return 8.0;
+		}
+	}
+
+	public double valorTotal() {
+		return couvert() + consumo() + ingresso();
+	}
+
 }
