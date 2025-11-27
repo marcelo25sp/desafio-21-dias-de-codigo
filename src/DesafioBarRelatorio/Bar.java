5757f1b402a3ad36b123bd185878aa834bebc0f2
Marcelo Nascimento <marcelo.ngomes@hotmail.com>
2025-11-27 19:14:16 -0300

    Adiciona comentários ao programa

--- src/DesafioBarRelatorio/Bar.java
+++ src/DesafioBarRelatorio/Bar.java
@@ -2,11 +2,13 @@ package DesafioBarRelatorio;
 
 public class Bar {
 
+	// atributos do programa
 	private char genero;
 	private int cerveja;
 	private int refrigerante;
 	private int espetinho;
 
+	
 	public Bar(char genero, int cerveja, int refrigerante, int espetinho) {
 		this.genero = genero;
 		this.cerveja = cerveja;
@@ -14,6 +16,7 @@ public class Bar {
 		this.espetinho = espetinho;
 	}
 
+	// métodos getters
 	public char getGenero() {
 		return genero;
 	}
@@ -29,7 +32,9 @@ public class Bar {
 	public int getEspetinho() {
 		return espetinho;
 	}
+	
 
+	// método que retorna um valor nulo caso o usuário tenha um consumo maior que R$ 30.00
 	public double couvert() {
 		if (consumo() >= 30.0) {
 			return 0.0;
@@ -38,10 +43,12 @@ public class Bar {
 		}
 	}
 
+	//método que retorna o valor total do consumo
 	public double consumo() {
 		return (cerveja * 5.0) + (refrigerante * 3.0) + (espetinho * 7.0);
 	}
 
+	// método que retorna um valor de acordo com o genero do cliente
 	public double ingresso() {
 		if (genero == 'M') {
 			return 10.0;
@@ -50,10 +57,12 @@ public class Bar {
 		}
 	}
 
+	// método que retorna o valor total a pagar
 	public double valorTotal() {
 		return couvert() + consumo() + ingresso();
 	}
 
+	
 	public String exibirRelatorio() {
 		return String.format("Consumo = R$ %.2f \nCouvert = R$ %.2f \nIngresso =  R$ %.2f\n", consumo(), couvert(),
 				ingresso());
--- src/DesafioBarRelatorio/Program.java
+++ src/DesafioBarRelatorio/Program.java
@@ -5,11 +5,23 @@ import java.util.Scanner;
 
 public class Program {
 
+	/*
+	 * Em um bar, o ingresso custa 10 reais para homens e 8 reais para mulheres.
+	 * Cada cerveja custa 5 reais, cada refrigerante 3 reais e cada espetinho custa
+	 * 7. Além disso, o bar cobra uma taxa de couvert artístico no valor de 4 reais,
+	 * porém, se o valor gasto com consumo for superior a 30 reais, o couvert
+	 * artístico não é cobrado. Fazer um programa para ler os seguintes dados de um
+	 * cliente do bar: sexo (F ou M), quantidade de cervejas, refrigerantes e
+	 * espetinhos consumidos. O programa deve então mostrar um relatório com a conta
+	 * a ser paga pelo cliente
+	 */
+
 	public static void main(String[] args) {
 
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
 
+		// entrada de dados do usuário
 		System.out.print("Genero:(M/F) ");
 		char genero = sc.next().charAt(0);
 		System.out.print("Quantidade de cervejas: ");
@@ -18,9 +30,11 @@ public class Program {
 		int refrigerantes = sc.nextInt();
 		System.out.print("Quantidade de espetinhos: ");
 		int espetinhos = sc.nextInt();
-		
+
+		// instancia da classe Bar
 		Bar cliente = new Bar(genero, cervejas, refrigerantes, espetinhos);
-		
+
+		// saída de dados e relatório da conta a pagar
 		System.out.println();
 		System.out.println("RELATÓRIO:");
 		System.out.println("-----------");
