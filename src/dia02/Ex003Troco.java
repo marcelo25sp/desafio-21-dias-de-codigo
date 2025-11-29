bbdc2053c746d27d79b1f5ccf0e4eabfd2f6c186
Marcelo Nascimento <marcelo.ngomes@hotmail.com>
2025-11-28 22:03:35 -0300

    Dia2 - Ex003Troco: cria a classe com estrutura inicial

---
+++ src/dia02/Ex003Troco.java
@@ -0,0 +1,17 @@
+package dia02;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Ex003Troco {
+
+	public static void main(String[] args) {
+		
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		sc.close();
+
+	}
+
+}
