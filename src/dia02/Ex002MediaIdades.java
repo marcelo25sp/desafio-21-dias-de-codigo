cd1f289f34dcf82bfae45b896c5a21eaea431d23
Marcelo Nascimento <marcelo.ngomes@hotmail.com>
2025-11-28 21:53:07 -0300

    Cria a estrutura para entrada de dados do usuário

--- src/dia02/Ex002MediaIdades.java
+++ src/dia02/Ex002MediaIdades.java
@@ -10,6 +10,15 @@ public class Ex002MediaIdades {
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
 		
+		System.out.print("Entre com a idade do usuário 1: ");
+		int idade1 = sc.nextInt();
+		System.out.print("Entre com a idade do usuário 2: ");
+		int idade2 = sc.nextInt();
+		System.out.print("Entre com a idade do usuário 3: ");
+		int idade3 = sc.nextInt();
+		
+		double media = (double)(idade1 + idade2 + idade3) / 3.0;
+		
 		sc.close();
 		
 	}
